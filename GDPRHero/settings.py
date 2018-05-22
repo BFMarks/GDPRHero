50d7ebc13f984e80ff10b501723c882c4b1cb898
bfmarks <bryanfmarks@gmail.com>
2018-05-21 21:38:28 -0700

    test

--- GDPRHero/settings.py
+++ GDPRHero/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,10 +29,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Application definition
 
@@ -110,6 +106,10 @@ WSGI_APPLICATION = 'GDPRHero.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# Heroku: Update database configuration from $DATABASE_URL.
+
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
