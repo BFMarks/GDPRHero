32662f919ab82a2a7d37ce1f1ddacca61cdca0c3
bfmarks <bryanfmarks@gmail.com>
2018-05-30 20:43:38 -0700

    removed pyc

--- GDPRHero/settings.py
+++ GDPRHero/settings.py
@@ -85,15 +85,22 @@ CHANNEL_LAYERS = {
     },
 }
 
-# CELERY_IMPORTS = ("tasks.py", )
+#FOR PRODUCTION
+CELERY_BROKER_URL=os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
 
+#FOR DEVELOPMENT 
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#MAINTAIN
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
+
+
+
 
 ROOT_URLCONF = 'GDPRHero.urls'
 
